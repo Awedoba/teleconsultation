87ffaf602077d7069e37b732a82dddff278ef393
Br0kinJester <50738523+Awedoba@users.noreply.github.com>
2021-07-05 10:08:19 +0000

    Initial commit

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>
+        @isset($title)
+        {{ $title }} |
+        @endisset
+        {{ config('app.name') }}
+    </title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-light">
+        <div id="root" class="container pt-lg-5 mt-lg-5">
+            @yield('content')
+        </div>
+</body>
+</html>
