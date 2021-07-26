8c55b16f2b051ce31b71c07c72e305809f2b5792
AlexOsei <dextroalex@gmail.com>
2021-07-26 12:21:53 +0000

    Added a footer

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -1,25 +1,37 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>
-        @isset($title)
-        {{ $title }} |
-        @endisset
-        TeleConsult
-    </title>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        <title>
+            @isset($title)
+            {{ $title }} |
+            @endisset
+            TeleConsult
+        </title>
+        <style>
+            .slap__bt-page{
+                width: 100%;
+                position: absolute;
+                bottom: 0;
+            }
+        </style>
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body class="bg-light">
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body class="bg-light">
         @include('layout.nav')
         <div id="root" class="container pt-lg-5 mt-lg-5">
             @yield('content')
         </div>
-</body>
+        @include('layout.footer')
+        {{-- <div id="root" class="container pt-lg-5 mt-lg-5">
+            @yield('content')
+        </div> --}}
+
+    </body>
 </html>
---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="text-center text-lg-start text-muted text-light slap__bt-page" style="background-color:#343a40">
+    <!-- Copyright -->
+    <div class="text-center p-4" style="color: white; font-size: 0.9rem;">
+        ©<script>document.write(new Date().getFullYear());</script>. All Rights Reserved. Copyright: GEMH IT
+    </div>
+    <!-- Copyright -->
+</footer>
