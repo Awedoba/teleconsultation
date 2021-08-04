7fc47541633dbd8f45d442e1ca3efef301f3b883
AlexOsei <dextroalex@gmail.com>
2021-08-04 09:47:19 +0000

    Added developer's contact to footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,10 @@
 <footer class="text-center text-lg-start text-muted text-light slap__bt-page" style="background-color:#343a40">
     <!-- Copyright -->
     <div class="text-center p-4" style="color: white; font-size: 0.9rem;">
-        ©<script>document.write(new Date().getFullYear());</script>. All Rights Reserved. Copyright: GEMH IT
+        ©<script>document.write(new Date().getFullYear());</script> by 
+        <strong>Severin and Alex</strong>
+        <br>0249538351 / 0553343640<br>
+        All Rights Reserved.
     </div>
     <!-- Copyright -->
 </footer>
